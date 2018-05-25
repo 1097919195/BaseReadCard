ef07f576eb799a0780b3ec71ecf9e82b311a3326
zjl7577 <1097919195@qq.com>
2018-05-25 13:46:23 +0800

    temp

---
+++ app/src/main/java/com/example/gxkj/cardnumbinding/fragment/SampleBindingFragment.java
@@ -0,0 +1,24 @@
+package com.example.gxkj.cardnumbinding.fragment;
+
+import com.jaydenxiao.common.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2018/5/25 0025.
+ */
+
+public class SampleBindingFragment extends BaseFragment{
+    @Override
+    protected int getLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
