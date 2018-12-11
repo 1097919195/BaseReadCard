e09e8406293bd698829df0e2ed4c87df7e1ce69d
zjl7577 <1097919195@qq.com>
2018-12-11 11:25:35 +0800

    登录字段优化

--- app/src/main/java/com/example/gxkj/cardnumbinding/bean/LoginTokenData.java
+++ app/src/main/java/com/example/gxkj/cardnumbinding/bean/LoginTokenData.java
@@ -13,7 +13,6 @@ public class LoginTokenData {
 
     private String access_token;
     private String token_type;
-    private int expires_in;
 
     public String getAccess_token() {
         return access_token;
@@ -31,11 +30,4 @@ public class LoginTokenData {
         this.token_type = token_type;
     }
 
-    public int getExpires_in() {
-        return expires_in;
-    }
-
-    public void setExpires_in(int expires_in) {
-        this.expires_in = expires_in;
-    }
 }
